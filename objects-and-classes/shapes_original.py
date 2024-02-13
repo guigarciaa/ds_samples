2b6831f918f8610e7ec83dba9839981fca5181d9
Guilherme Garcia <guigarcia.a@outlook.com>
2024-02-13 09:46:42 -0300

    Finish script for understand inhence and polimorfism using python.

--- objects-and-classes/shapes_original.py
+++ objects-and-classes/shapes_original.py
@@ -1,3 +1,6 @@
+import math
+
+# The Shape Class is the base class for the Square and Circle classes
 class Shape:
     def __init__(self, name):
         self.name = name
@@ -7,7 +10,36 @@ class Shape:
     
     def area(self):
         raise NotImplementedError("Area not implemented")
+
+# The Square Class Inhence of Shape and Polimofysm implement the area and perimeter  
+class Square(Shape):
+    def __init__(self, name, side):
+        super().__init__(name)
+        self.side = side
+
+    def perimeter(self):
+        return 4 * self.side
+
+    def area(self):
+        return self.side ** 2
+
+# The Circle Class Inhence of Shape and Polimofysm implement the area and perimeter
+class Circle(Shape):
+    def __init__(self, name, side):
+        super().__init__(name)
+        self.side = side
+
+    def perimeter(self):
+        return 2 * math.pi * self.side
+
+    def area(self):
+        return self.side ** 2
     
 
 
-print(Shape("circle").perimeter())  # NotImplementedError: Perimeter not implemented
+examples = [Square("sq", 3), Circle("ci", 2)]
+for thing in examples:
+    n = thing.name
+    p = thing.perimeter()
+    a = thing.area()
+    print(f"{n} has perimeter {p:.2f} and area {a:.2f}")
