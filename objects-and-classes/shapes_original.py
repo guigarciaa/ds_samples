2bfa32738e61e6d0ae487915b6a7b5c438462658
Guilherme Garcia <guigarcia.a@outlook.com>
2024-02-13 09:05:24 -0300

    create the first script in python

---
+++ objects-and-classes/shapes_original.py
@@ -0,0 +1,13 @@
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+    def perimeter(sefl):
+        raise NotImplementedError("Perimeter not implemented")
+    
+    def area(self):
+        raise NotImplementedError("Area not implemented")
+    
+
+
+print(Shape("circle").perimeter())  # NotImplementedError: Perimeter not implemented
